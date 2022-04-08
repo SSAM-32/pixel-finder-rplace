d0c8f042c6def39536f0d18190d790d358faa0c6
SSAM-32 <103268810+SSAM-32@users.noreply.github.com>
2022-04-08 12:03:24 -0300

    Update placepixel.py

--- placepixel.py
+++ placepixel.py
@@ -1,7 +1,7 @@
 import csv
 
-FilePath = "/home/ssam/Documents/misc/place/2022_place_canvas_history.csv"
-coords = "122,706"
+FilePath = ""
+coords = ""
 
 with open(FilePath) as file:
     reader = csv.reader(file, delimiter =',')
